b107d761796a7c6dbcea4719b21ad510b8382223
Eve Redero <eve.redero@gmail.com>
2025-05-03 22:07:56 +0200

    dbg: connect reset option

--- boards/rederotech/asynthosc/board.cmake
+++ boards/rederotech/asynthosc/board.cmake
@@ -4,8 +4,7 @@
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(openocd --cmd-post-verify "reset halt")
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
-board_runner_args(jlink "--device=STM32H743VI" "--speed=4000")
-board_runner_args(pyocd "--target=stm32h743vitx")
+board_runner_args(blackmagicprobe "--connect-rst")
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
