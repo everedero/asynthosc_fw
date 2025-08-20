5c08444b6bbbca319fd5eb9f873727d489da6a08
Eve Redero <eve.redero@gmail.com>
2025-08-20 15:30:33 +0200

    sample: osc: buffer instead of standalone

--- samples/osc_sock_client/src/create_osc_pack.c
+++ samples/osc_sock_client/src/create_osc_pack.c
@@ -10,19 +10,29 @@
  * A basic program to listen to port 9000 and print received OSC packets.
  */
 int create_msg(char *tx_buffer, int max_len) {
+      tosc_bundle bundle;
+    int len = 0;
+      // Test timestamp 0xC007CAFE
+      tosc_writeBundle(&bundle, 0xC007CAFE, tx_buffer, max_len);
+      tosc_writeNextMessage(&bundle, "/a", "i", 1);
+      tosc_writeNextMessage(&bundle, "/toto", "s", "hello!");
 
-	  printk("Starting write tests:\n");
-	  int len = 0;
-	  char blob[8] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
-	  //len = tosc_writeMessage(tx_buffer, max_len, "/address", "f", 1.0);
-	  len = tosc_writeMessage(tx_buffer, max_len, "/address", "fs", 2.0, "Hello");
-	  printk("Print osc:\n");
-	  tosc_printOscBuffer(tx_buffer, len);
-	  return(len);
+      len = tosc_getBundleLength(&bundle);
+      printk("Starting write tests:\n");
+      printk("Print osc:\n");
+      tosc_printOscBuffer(tx_buffer, len);
+      return(len);
 }
 void read_msg(char* buffer, int len) {
-	  tosc_message osc;
-	  tosc_parseMessage(&osc, buffer, len);
-	  printk("Print msg:\n");
-	  tosc_printMessage(&osc);
+      tosc_bundle bundle;
+      tosc_message msg;
+      bool is_msg = true;
+      tosc_parseBundle(&bundle, buffer, len);
+      is_msg = tosc_getNextMessage(&bundle, &msg);
+      while (is_msg) {
+          tosc_parseMessage(&msg, buffer, len);
+          printk("Print msg:\n");
+          tosc_printMessage(&msg);
+          is_msg = tosc_getNextMessage(&bundle, &msg);
+    }
 }
